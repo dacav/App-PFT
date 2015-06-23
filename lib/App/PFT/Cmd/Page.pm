e33cfe6986a04dbeeb258cd514952a62e374ec01
Giovanni Simoni <simgidacav@gmail.com>
2015-06-23 20:11:02 +0200

    page command

--- lib/App/PFT/Cmd/Page.pm
+++ lib/App/PFT/Cmd/Page.pm
@@ -19,17 +19,44 @@ use feature qw/say/;
 use Pod::Usage;
 use Pod::Find qw/pod_where/;
 
-use Carp;
+use App::PFT::Struct::Tree;
+use App::PFT::Data::Date;
 
-sub help {
-    pod2usage
-        -exitval => 1,
-        -verbose => 2,
-        -input => pod_where({-inc => 1}, __PACKAGE__)
-    ;
-}
+use Getopt::Long qw/GetOptionsFromArray/;
+
+Getopt::Long::Configure qw/bundling/;
+
+use App::PFT::Struct::Conf qw/$ROOT $AUTHOR/;
 
 sub main {
+    my %opts = (
+        author => $AUTHOR,
+    );
+    my %datespec;
+    GetOptions(
+        'hide=s'        => \$opts{hide},
+        'author|a=s'    => \$opts{author},
+        'help|h!'       => sub {
+            pod2usage
+                -exitval => 1,
+                -verbose => 2,
+                -input => pod_where({-inc => 1}, __PACKAGE__)
+            ;
+        },
+    );
+
+    unless (@ARGV) {
+        say STDERR "Usage: $0 page [options] <title>";
+        exit 1;
+    }
+
+    my $tree = App::PFT::Struct::Tree->new(basepath => $ROOT);
+    my $page = $tree->page(
+        title => join(' ', @ARGV),
+        %opts
+    );
+
+    $page->edit;
 }
 
 1;
