5a29f05894ea68b3db4247059609752075d1b629
Giovanni Simoni <simgidacav@gmail.com>
2015-08-15 22:40:12 +0200

    Constants

--- lib/App/PFT.pm
+++ lib/App/PFT.pm
@@ -6,7 +6,10 @@ use warnings;
 use Exporter 'import';
 our @EXPORT_OK = qw/$Name $ConfName/;
 
+# Application Constants
+
 our $Name = 'pft';
 our $ConfName = 'pft.yaml';
+our $NoInitMsg = "Not a $Name site. Try running: $Name init";
 
 1;
