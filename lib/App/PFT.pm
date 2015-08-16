b1a1ae378f0a2e863951d4dffc5eb03d3f34c7eb
Giovanni Simoni <simgidacav@gmail.com>
2015-08-16 15:21:54 +0200

    pft-* subcommands

--- lib/App/PFT.pm
+++ lib/App/PFT.pm
@@ -4,12 +4,25 @@ use strict;
 use warnings;
 
 use Exporter 'import';
-our @EXPORT_OK = qw/$Name $ConfName/;
+our @EXPORT_OK = qw/$Name $ConfName findroot/;
+
+use App::PFT::Util;
+use Carp;
 
 # Application Constants
 
 our $Name = 'pft';
 our $ConfName = 'pft.yaml';
 our $NoInitMsg = "Not a $Name site. Try running: $Name init";
+
+sub findroot {
+    my %opts = @_;
+    my $out = App::PFT::Util::findroot(
+        type => 'file',
+        name => $ConfName
+    );
+    croak $NoInitMsg if (!defined($out) and $opts{'-die'});
+    $out;
+}
 
 1;
