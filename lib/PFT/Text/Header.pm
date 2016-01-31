9dca58b36dc0995a3f06dc5148e95d6d35278e45
Giovanni Simoni <dacav@openmailbox.org>
2016-01-31 20:25:02 +0100

    Clean header creation

--- lib/PFT/Text/Header.pm
+++ lib/PFT/Text/Header.pm
@@ -16,17 +16,18 @@ PFT::Text::Header - Header for PFT content textfiles
 
     use PFT::Text::Header;
 
-    PFT::Text::Header->load(STDIN);
-    PFT::Text::Header->load('/path/to/file');
-
-    PFT::Text::Header->new(
-        title => $title,        # mandatory, encoded form
-        author => $author,      # optional, encoded form
+    my $hdr = PFT::Text::Header->new(
+        title => $title,        # mandatory
+        author => $author,
         template => $template,
         encoding => $encoding,  # defaults 'utf-8'
-        tags => $tags,          # list of encoded bytes, defaults []
+        tags => $tags,          # defaults []
         opts => $opts,          # defaults {}
     );
+
+    my $hdr = PFT::Text::Header->load(STDIN);
+
+    my $hdr = PFT::Text::Header->load('/path/to/file');
 
 =head1 DESCRIPTION
 
@@ -41,7 +42,11 @@ expected to be a list of encoded strings.
 
 =cut
 
-use Encode 'decode';
+use Encode qw/encode decode/;
+use Carp;
+use YAML::Tiny;
+
+our $DEFAULT_ENC = 'utf-8';
 
 sub new {
     my $cls = shift;
@@ -49,23 +54,107 @@ sub new {
 
     exists $opts{title} or die 'title is mandatory';
 
-    my $enc = $opts{encoding} || 'utf-8';
-    my $dec = sub { decode $enc, shift };
+    my $enc = $opts{encoding} || $DEFAULT_ENC;
 
     bless {
-        title => $dec->($opts{title}),
-        author => $dec->($opts{author}),
+        title => $opts{title},
+        author => $opts{author},
         template => $opts{template},
         encoding => $enc,
-        tags => map { $dec->($_) } ($opts{tags} || []),
+        tags => $opts{tags} || [],
         opts => $opts{opts} || {},
     }, $cls;
 }
+
+sub dump {
+    my $self = shift;
+    my $to = shift;
+
+    my $type = ref $to;
+    if ($type ne 'GLOB' && $type ne 'IO::File') {
+        confess "Only supporting GLOB and IO::File. Got ",
+                $type ? $type : 'Scalar'
+    }
+    my $tags = $self->tags;
+    print $to encode($self->encoding, YAML::Tiny::Dump {
+        Title => $self->title,
+        Author => $self->author,
+        Encoding => $self->encoding,
+        Template => $self->template,
+        Tags => @$tags ? $tags : undef,
+        Options => $self->opts,
+    }), "---\n"
+}
+
+sub load {
+    my $cls = shift;
+    my $from = shift;
+
+    my $hdr = do {
+        # Header starts with a valid YAML document (including the leading
+        # /^---$/ string) and ends with another /^---$/ string.
+
+        my $text;
+        my $type = ref $from;
+        if ($type eq 'GLOB' || $type eq 'IO::File') {
+            $text = <$from>;
+            while (<$from>) {
+                last if ($_ =~ /^---$/);
+                $text .= $_;
+            }
+        } else {
+            croak "Only supporting GLOB and IO::File. Got $type" if $type;
+        }
+        eval { YAML::Tiny::Load($text) };
+    };
+    croak $@ =~ s/ at .*$//rs if $@;
+
+    my $enc = $hdr->{Encoding} || $DEFAULT_ENC;
+
+    my $decode = sub {
+        my $v = $hdr->{$_[0]};
+        croak "Conf '$_[0]' is mandatory" if $_[1] && !defined $v;
+        croak "Conf '$_[0]' must be a string" if ref $v;
+        decode($enc, $v)
+    };
+
+    bless {
+        title => $decode->(Title => 1),
+        author => $decode->(Author => 0),
+        template => exists $hdr->{Template}
+            ? $decode->(Template => 0)
+            : undef,
+        tags => do {
+            my $tags = $hdr->{Tags};
+            ref $tags eq 'ARRAY' ? $tags
+                : defined $tags ? [$tags]
+                : []
+        },
+        encoding => $enc,
+        opts => exists $hdr->{Options}
+            ? $hdr->{Options}
+            : undef,
+    }, $cls;
+}
+
+=pod
+
+Available fields:
+
+    $hdr->title
+    $hdr->author
+    $hdr->template
+    $hdr->encoding
+    $hdr->tags
+    $hdr->opts
+
+=cut
 
 sub title { shift->{title} }
 sub author { shift->{author} }
 sub template { shift->{template} }
 sub encoding { shift->{encoding} }
+sub tags { shift->{tags} }
 sub opts { shift->{opts} }
 
 1;
--- t/check-header.t
+++ t/check-header.t
@@ -3,23 +3,30 @@
 use warnings;
 use strict;
 
+use v5.10;
+
 use feature qw/say/;
-use Test::More tests => 2;
+use Test::More tests => 1;
 
 use Encode qw/encode decode/;
+use File::Temp qw/tempfile tempdir/;
 
 use PFT::Text::Header;
 
 my $h = PFT::Text::Header->new(
-    title => encode('utf-8', 'Rådmansgatan'),
-);
-cmp_ok($h->title, 'eq', 'Rådmansgatan');
-
-$h = PFT::Text::Header->new(
-    title => encode('iso8859-15', 'Rådmansgatan'),
+    title => 'Rådmansgatan',
     encoding => 'iso8859-15',
 );
-cmp_ok(decode('iso8859-15', $h->title), 'eq', 'Rådmansgatan');
 
+my $dir = tempdir(CLEANUP => 1);
+my($fh, $filename) = tempfile(DIR => $dir);
+
+$h->dump($fh);
+print $fh 'Hello';
+seek $fh, 0, 0;
+my $hl = PFT::Text::Header->load($fh);
+close $fh;
+
+is_deeply($h, $h, 'dump and reload');
 
 done_testing()
