8aea7d44f6492da3b40910ffcaf2a1d8fe029cdb
Giovanni Simoni <dacav@openmailbox.org>
2016-02-07 16:29:58 +0100

    Read page.

--- lib/PFT/Content/Page.pm
+++ lib/PFT/Content/Page.pm
@@ -40,8 +40,9 @@ header is broken returns undef and sets $@.
 =cut
 
 use parent 'PFT::Content::File';
-
 use PFT::Text::Header;
+
+use Carp;
 
 sub header {
     my $self = shift;
@@ -51,6 +52,32 @@ sub header {
     $@ =~ s/ at .*$// if $@;
     $h;
 }
+
+=item read
+
+Read the page. In scalar context returns an open file descriptor
+configured with the correct `binmode` according to the header.
+
+In list context returns the header and the same descriptor. Returns undef
+if the file does not exist.
+
+Croaks if the header is broken.
+
+=cut
+
+sub read {
+    my $self = shift;
+
+    return undef unless $self->exists;
+    my $fh = $self->open('r');
+    my $h = eval { PFT::Text::Header->load($fh) };
+    croak $@ =~ s/ at .*$//rs if $@;
+    binmode $fh, ':encoding(' . $h->encoding . ')';
+
+    wantarray ? ($h, $fh) : $fh;
+}
+
+=pod
 
 =back
 
--- t/check-files.t
+++ t/check-files.t
@@ -6,9 +6,11 @@ use strict;
 use warnings;
 use utf8;
 
+use feature qw/say/;
+
 use PFT::Content::Page;
 
-use Test::More tests => 4;
+use Test::More;
 use File::Temp;
 use File::Spec;
 
@@ -24,6 +26,12 @@ is($page->header, undef, 'Empty file has no header');
 is($@, '', 'But also no error');
 
 do {
+    my($h, $text) = $page->read();
+    is($h, undef, 'Read goes undef 1');
+    is($text, undef, 'Read goes undef 2');
+};
+
+do {
     my $fh = $page->open('w');
     print $fh 'Hello';
 };
@@ -31,5 +39,10 @@ do {
 is($page->header, undef, 'Arbitrary text has no header');
 isnt($@, undef, 'Error instead');
 diag('Error was: ', $@);
+
+eval {
+    my($h, $text) = $page->read();
+};
+isnt($@, undef, 'Error also if reading';
 
 done_testing()
