0328ab7d8a6b50750c8950d77ae48d938b047429
Giovanni Simoni <dacav@openmailbox.org>
2016-02-24 16:28:58 +0100

    Top tree

---
+++ lib/PFT/Tree.pm
@@ -0,0 +1,74 @@
+package PFT::Tree v0.0.1;
+
+use v5.10;
+
+use strict;
+use warnings;
+use utf8;
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+PFT::Tree - Filesystem tree mapping a PFT site
+
+=head1 SYNOPSIS
+
+    PFT::Tree->new($basedir);
+
+=head1 DESCRIPTION
+
+=cut
+
+use File::Spec;
+use File::Path qw/make_path/;
+
+use Carp;
+
+sub new {
+    my $cls = shift;
+
+    my $self = bless { base => shift }, $cls;
+    $self->_init();
+    $self->{content} = PFT::Content->new($self->dir_content);
+
+    $self
+}
+
+sub _init {
+    my $self = shift;
+    make_path map({ $self->$_ } qw/
+        dir_content
+        dir_backends
+    /), {
+        #verbose => 1,
+        mode => 0711,
+    }
+}
+
+=head2 Properties
+
+=over 1
+
+=item dir_content
+
+=cut
+
+sub dir_root { base }
+sub dir_content { File::Spec->catdir(shift->{base}, 'content') }
+sub dir_backends { File::Spec->catdir(shift->{base}, 'templates') }
+sub dir_inject { File::Spec->catdir(shift->{base}, 'templates') }
+
+=item content
+
+=cut
+
+sub content { shift->{content} }
+
+=back
+
+=cut
+
+1;
