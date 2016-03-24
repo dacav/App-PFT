b68f137ba1a9c0a35007c7eac554d2289a921df1
Giovanni Simon <giovanni.simoni@schibsted.com>
2016-03-24 20:11:40 +0100

    Fixing help lookup

--- lib/App/PFT.pm
+++ lib/App/PFT.pm
@@ -10,4 +10,11 @@ our $Name = 'pft';
 our $ConfName = 'pft.yaml';
 our $NoInitMsg = "Not a $Name site. Try running: $Name init";
 
+use FindBin;
+use File::Spec;
+
+sub help_of {
+    File::Spec->catfile($FindBin::RealBin, join '-', $Name, @_);
+}
+
 1;
