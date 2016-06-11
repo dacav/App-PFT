17285ef220b7edf7aa9738b498f39bde5029cde7
Giovanni Simoni <dacav@openmailbox.org>
2016-06-11 22:32:12 +0200

    Version as pft --version

--- lib/App/PFT.pm
+++ lib/App/PFT.pm
@@ -4,11 +4,17 @@ use strict;
 use warnings;
 
 use Exporter 'import';
-our @EXPORT_OK = qw/$Name $ConfName/;
+our @EXPORT_OK = qw/$Name $ConfName $VersionString/;
 
 our $Name = 'pft';
 our $ConfName = 'pft.yaml';
 our $NoInitMsg = "Not a $Name site. Try running: $Name init";
+our $VersionString = <<EOF;
+PFT v 1.0.1  Copyright (C) 2016  Giovanni Simoni
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it
+under certain conditions; see the source code for details.
+EOF
 
 use FindBin;
 use File::Spec;
